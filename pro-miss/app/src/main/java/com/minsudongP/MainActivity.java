42167dcf0bb9d31c1e5505ac05cedf128fdecf3f
tngusmiso <k1miso012@gmail.com>
2019-03-20 16:33:06 +0900

    마이페이지 프론트 (친구목록은 미완성)

--- pro-miss/app/src/main/java/com/minsudongP/MainActivity.java
+++ pro-miss/app/src/main/java/com/minsudongP/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,8 +26,20 @@ public class MainActivity extends AppCompatActivity {
                 //되돌아 올수 있어 finish()를 하지 않음
             }
         };
+
+        View.OnClickListener MyPageListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MyPageActivity.class);
+                startActivity(intent);
+            }
+        };
+
+
         ((ImageButton)findViewById(R.id.main_MakeProtocal)).setOnClickListener(MakeProtocalListenr);
         ((TextView)findViewById(R.id.main_MakeProtocal_text)).setOnClickListener(MakeProtocalListenr);
+        ((LinearLayout)findViewById(R.id.main_MyPage)).setOnClickListener(MyPageListener);
+        ((TextView)findViewById(R.id.main_MyPage_text)).setOnClickListener(MyPageListener);
 
     }
 }
