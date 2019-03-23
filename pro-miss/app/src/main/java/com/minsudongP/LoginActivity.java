bd14cfa5bb96b634f82f4b9ee0e941f156f572f9
jaeryo2357 <jaeryo2357@naver.com>
2019-03-23 15:27:53 +0900

    url 통신 확인

--- pro-miss/app/src/main/java/com/minsudongP/LoginActivity.java
+++ pro-miss/app/src/main/java/com/minsudongP/LoginActivity.java
@@ -3,8 +3,17 @@ package com.minsudongP;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.minsudongP.Singletone.UrlConnection;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +30,30 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ((Button)findViewById(R.id.login_login)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread() {
+                    public void run() {
+                        // 파라미터 2개와 미리정의해논 콜백함수를 매개변수로 전달하여 호출
+                        UrlConnection.shardUrl.GetRequest("데이터2", new Callback() {
+                            @Override
+                            public void onFailure(Call call, IOException e) {
+
+                            }
+
+                            @Override
+                            public void onResponse(Call call, Response response) throws IOException {
+                                Log.d("urlTest",response.body().string());
+                            }
+                        },null);
+
+                    }
+                }.start();
+            }
+        });
+
 
 
 
--- pro-miss/app/src/main/java/com/minsudongP/Singletone/UrlConnection.java
+++ pro-miss/app/src/main/java/com/minsudongP/Singletone/UrlConnection.java
@@ -1,19 +1,52 @@
 package com.minsudongP.Singletone;
 
+import java.util.HashMap;
+
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class UrlConnection {
     private OkHttpClient client;
     public static UrlConnection shardUrl=new UrlConnection();
 
 
-    private String url;
+    private String Mainurl="http://106.10.58.28/";
 
     private UrlConnection(){
         this.client=new OkHttpClient();
     }
 
-    
+    public void GetRequest(String PHP,  Callback callback, HashMap<String,String> map)
+    {
+        String url=Mainurl;
+
+        Request request=new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(callback);
+    }
+
+    public void PostRequest(String PHP,  Callback callback, HashMap<String,String> map)
+    {
+
+    }
+
+    public void DeleteRequest(String PHP,  Callback callback, HashMap<String,String> map)
+    {
+
+    }
+
+    public void PutRequest(String PHP,  Callback callback, HashMap<String,String> map)
+    {
+
+    }
+
+
+
+
 
 
 }
