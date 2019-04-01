f660d0107bc08876cfc982118eae78718f6a4740
jaeryo2357 <jaeryo2357@naver.com>
2019-04-01 20:21:57 +0900

    음성인식 test

--- pro-miss/app/src/main/java/com/minsudongP/MainActivity.java
+++ pro-miss/app/src/main/java/com/minsudongP/MainActivity.java
@@ -1,13 +1,33 @@
 package com.minsudongP;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaDataSource;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.minsudongP.Singletone.UrlConnection;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +47,89 @@ public class MainActivity extends AppCompatActivity {
                 //되돌아 올수 있어 finish()를 하지 않음
             }
         };
+
+
+        findViewById(R.id.main_notify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new Thread() {
+
+                    @Override
+                    public void run() {
+                        UrlConnection connection= UrlConnection.shardUrl;
+                        connection.PostSpeekRequest("음성 테스트입니다. 남수형 바보",new
+
+                                Callback() {
+                                    @Override
+                                    public void onFailure (Call call, IOException e){
+
+                                    }
+
+                                    @Override
+                                    public void onResponse (Call call, Response response) throws IOException {
+
+                                        Log.d("code",""+response.code());
+                                        if (response.code() == 200) {
+
+/*
+                                            InputStream is = response.body().byteStream();
+                                            int read = 0;
+                                            byte[] bytes = new byte[1024];
+                                            // Creates randomly named MP3 files
+                                           // String tempname = Long.valueOf(new Date().getTime()).toString();
+                                            File f = new File(getApplicationContext().getFilesDir().getPath().toString()+ "speek"+ ".mp3");
+                                            f.createNewFile();
+                                            OutputStream outputStream = new FileOutputStream(f);
+                                            while ((read = is.read(bytes)) != -1) {
+                                                outputStream.write(bytes, 0, read);
+                                            }
+                                            outputStream.flush();
+                                            outputStream.close();
+                                            is.close();
+                                            */
+
+                                            byte[] bytes=response.body().bytes();
+                                            Log.d("code",""+bytes.length);
+                                            File f = new File(getApplicationContext().getFilesDir().getPath().toString()+ "speek"+ ".mp3");
+
+                                            FileOutputStream outputStream = new FileOutputStream(f);
+
+                                            outputStream.write(bytes,0,bytes.length);
+                                            outputStream.flush();
+                                            outputStream.close();
+
+                                            FileInputStream fs = new FileInputStream(f);
+
+
+
+                                            MediaPlayer mediaPlayer = new MediaPlayer();
+                                            mediaPlayer.setDataSource(fs.getFD());
+                                            mediaPlayer.prepare();
+                                            fs.close();
+                                            // mediaPlayer.prepareAsync();
+                                            mediaPlayer.start();
+//                                            final SoundPool soundPool=new SoundPool(1, AudioManager.STREAM_MUSIC,0);
+//
+//
+//                                            final int music=soundPool.load(outputStream.getFD(),0,f.length(),1);
+//                                           // int music=soundPool.load("speek.mp3",1);
+//
+//
+//                                            MainActivity.this.runOnUiThread(new Runnable() {
+//                                                @Override
+//                                                public void run() {
+//                                                    soundPool.play(music, 1f, 1f, 1, 0, 1f);
+//                                                }
+//                                            });
+
+                                        }
+                                    }
+                                });
+                    }
+                }.start();
+            }
+        });
 
         View.OnClickListener MyPageListener=new View.OnClickListener() {
             @Override
--- pro-miss/app/src/main/java/com/minsudongP/Singletone/UrlConnection.java
+++ pro-miss/app/src/main/java/com/minsudongP/Singletone/UrlConnection.java
@@ -1,8 +1,11 @@
 package com.minsudongP.Singletone;
 
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -13,6 +16,7 @@ public class UrlConnection {
 
 
     private String Mainurl="http://106.10.58.28/";
+    private String SpeekUrl="https://naveropenapi.apigw.ntruss.com/voice/v1/tts";
 
     private UrlConnection(){
         this.client=new OkHttpClient();
@@ -41,6 +45,30 @@ public class UrlConnection {
 
     public void PutRequest(String PHP,  Callback callback, HashMap<String,String> map)
     {
+
+    }
+
+    public void PostSpeekRequest(String Text,Callback callback)
+    {
+
+
+
+
+            FormBody body = new FormBody.Builder()
+                    .add("speaker", "mijin")
+                    .add("speed", "0")
+                    .add("text", Text)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .addHeader("Content-Type","application/x-www-form-urlencoded")
+                    .addHeader("charset","UTF-8")
+                    .addHeader("X-NCP-APIGW-API-KEY-ID", "733fhqom8j")
+                    .addHeader("X-NCP-APIGW-API-KEY", "jwxEUZxCO1lKD0c1R1CDcahGlgm4iy7jLZHVRaAP")
+                    .url(SpeekUrl)
+                    .post(body)
+                    .build();
+            client.newCall(request).enqueue(callback);
 
     }
 
