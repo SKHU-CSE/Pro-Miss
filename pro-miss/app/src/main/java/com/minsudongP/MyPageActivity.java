e730cc375feaee83c3293ef610726ec47bc9e169
tngusmiso <k1miso012@gmail.com>
2019-05-24 20:49:30 +0900

    약속 달성률을 클릭하면 지난 약속 액티비티(PastActivity)로 이동

--- pro-miss/app/src/main/java/com/minsudongP/MyPageActivity.java
+++ pro-miss/app/src/main/java/com/minsudongP/MyPageActivity.java
@@ -29,29 +29,39 @@ import okhttp3.Response;
 public class MyPageActivity extends AppCompatActivity {
 
     AllRecyclerAdapter adapter;
-    ArrayList<PromissItem> arrayList=new ArrayList<>();
+    ArrayList<PromissItem> arrayList = new ArrayList<>();
     TextView money;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
-        RecyclerView recyclerView=findViewById(R.id.mypage_recycleview);
+        RecyclerView recyclerView = findViewById(R.id.mypage_recycleview);
 
-        final UserInfor infor= UserInfor.shared;
+        final UserInfor infor = UserInfor.shared;
 
-        money=findViewById(R.id.mypage_money_value);
-        ((TextView)findViewById(R.id.mypage_name)).setText(infor.getName());
-         money.setText(infor.getMoney()+"원");
-        if(infor.getAppoint_num()!=0) {
+        money = findViewById(R.id.mypage_money_value);
+        ((TextView) findViewById(R.id.mypage_name)).setText(infor.getName());
+        money.setText(infor.getMoney() + "원");
+
+        if (infor.getAppoint_num() != 0) {
             ((TextView) findViewById(R.id.mypage_rangeNum_text)).setText("" + (infor.getSuccess_appoint_num() / infor.getAppoint_num() * 100) + "%");
             ((TextView) findViewById(R.id.mypage_rangePercent_text)).setText(infor.getSuccess_appoint_num() + "/" + infor.getAppoint_num());
-            ((ProgressBar)findViewById(R.id.mypage_range_progress)).setProgress((infor.getSuccess_appoint_num()/infor.getAppoint_num())*100);
-        }else
-        {
+            ((ProgressBar) findViewById(R.id.mypage_range_progress)).setProgress((infor.getSuccess_appoint_num() / infor.getAppoint_num()) * 100);
+        } else {
             ((TextView) findViewById(R.id.mypage_rangeNum_text)).setText("0%");
             ((TextView) findViewById(R.id.mypage_rangePercent_text)).setText("0/0");
-            ((ProgressBar)findViewById(R.id.mypage_range_progress)).setProgress(0);
+            ((ProgressBar) findViewById(R.id.mypage_range_progress)).setProgress(0);
         }
+
+        // 약속 달성률 버튼 클릭 시 -> 지난 약속 목록으로 이동
+        findViewById(R.id.mypage_range_cardview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyPageActivity.this, PastActivity.class);
+                startActivity(intent);
+            }
+        });
 
         findViewById(R.id.mypage_money_addbtn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,95 +81,91 @@ public class MyPageActivity extends AppCompatActivity {
             }
         });
 
-
-        View.OnClickListener AttendingListener=new View.OnClickListener() {
+        View.OnClickListener AttendingListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MyPageActivity.this,AttendingActivity.class);
+                Intent intent = new Intent(MyPageActivity.this, AttendingActivity.class);
                 startActivity(intent);
             }
         };
 
-        View.OnClickListener SettingsListener=new View.OnClickListener() {
+        View.OnClickListener SettingsListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MyPageActivity.this,SettingsActivity.class);
+                Intent intent = new Intent(MyPageActivity.this, SettingsActivity.class);
                 startActivity(intent);
             }
         };
 
-        View.OnClickListener PastListener=new View.OnClickListener() {
+        View.OnClickListener PastListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MyPageActivity.this,PastActivity.class);
+                Intent intent = new Intent(MyPageActivity.this, PastActivity.class);
                 startActivity(intent);
             }
         };
 
-        View.OnClickListener MainListener=new View.OnClickListener() {
+        View.OnClickListener MainListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         };
-        ((Button)findViewById(R.id.mypage_backButton)).setOnClickListener(MainListener);
+        ((Button) findViewById(R.id.mypage_backButton)).setOnClickListener(MainListener);
 
 
-        adapter=new AllRecyclerAdapter(arrayList,MyPageActivity.this);
+        adapter = new AllRecyclerAdapter(arrayList, MyPageActivity.this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
-        ((Button)findViewById(R.id.mypage_attendingButton)).setOnClickListener(AttendingListener);
-        ((Button)findViewById(R.id.mypage_setButton)).setOnClickListener(SettingsListener);
-        ((Button)findViewById(R.id.mypage_pastButton)).setOnClickListener(PastListener);
+        ((Button) findViewById(R.id.mypage_attendingButton)).setOnClickListener(AttendingListener);
+        ((Button) findViewById(R.id.mypage_setButton)).setOnClickListener(SettingsListener);
+        ((Button) findViewById(R.id.mypage_pastButton)).setOnClickListener(PastListener);
 
+        arrayList.add(new PromissItem(PromissType.FriendLIst, "urltest", "양민욱"));
 
-        arrayList.add(new PromissItem(PromissType.FriendLIst,"urltest","양민욱"));
+        arrayList.add(new PromissItem(PromissType.FriendLIst, "urltest", "구동섭"));
 
-        arrayList.add(new PromissItem(PromissType.FriendLIst,"urltest","구동섭"));
-
-        arrayList.add(new PromissItem(PromissType.FriendLIst,"urltest","김종인"));
+        arrayList.add(new PromissItem(PromissType.FriendLIst, "urltest", "김종인"));
 
         adapter.notifyDataSetChanged();
     }
 
 
-    private Callback callback=new Callback() {
+    private Callback callback = new Callback() {
         @Override
         public void onFailure(Call call, IOException e) {
             MyPageActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    Toast.makeText(MyPageActivity.this,"네트워크를 확인해주세요.",Toast.LENGTH_LONG).show();
+                    Toast.makeText(MyPageActivity.this, "네트워크를 확인해주세요.", Toast.LENGTH_LONG).show();
                 }
             });
         }
 
         @Override
         public void onResponse(Call call, Response response) throws IOException {
-            String s=response.body().string();
+            String s = response.body().string();
 
             try {
-                final JSONObject jsonObject=new JSONObject(s);
-                if(jsonObject.getInt("result")==1000)
-                {
+                final JSONObject jsonObject = new JSONObject(s);
+                if (jsonObject.getInt("result") == 1000) {
                     MyPageActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             try {
-                                Toast.makeText(MyPageActivity.this,jsonObject.getString("message"),Toast.LENGTH_LONG).show();
+                                Toast.makeText(MyPageActivity.this, jsonObject.getString("message"), Toast.LENGTH_LONG).show();
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
                         }
                     });
-                }else
-                {
-                    final JSONObject object=jsonObject.getJSONObject("data");
+                } else {
+                    final JSONObject object = jsonObject.getJSONObject("data");
                     MyPageActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             try {
-                                money.setText(object.getString("money")+"원");
+                                money.setText(object.getString("money") + "원");
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
