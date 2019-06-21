592bd5831f4319f44cb8f830a2e98eda865058af
tngusmiso <k1miso012@gmail.com>
2019-06-21 20:36:09 +0900

    날짜, 시간, 타이머 설정할 때 현재시간 이전으로 선택 방지하기

--- pro-miss/app/src/main/java/com/minsudongP/DialogSelectTimer.java
+++ pro-miss/app/src/main/java/com/minsudongP/DialogSelectTimer.java
@@ -1,5 +1,6 @@
 package com.minsudongP;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
@@ -21,7 +22,7 @@ public class DialogSelectTimer {
     }
 
     // 호출할 다이얼로그 함수를 정의한다.
-    public void callFunction(final TextView timer_label, final TextView upper_label, final TextView plus_label) {
+    public void callFunction(final TextView timer_label, final TextView upper_label, final TextView plus_label, final int least) {
 
         // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
         final Dialog dlg = new Dialog(context);
@@ -41,8 +42,8 @@ public class DialogSelectTimer {
         final Button okButton = (Button) dlg.findViewById(R.id.dialog_okButton);
 
 
-        ArrayAdapter<String> Houradapter=new ArrayAdapter<String>(context,R.layout.spinnertext,context.getResources().getStringArray(R.array.hour));
-        ArrayAdapter<String> Minadapter=new ArrayAdapter<String>(context,R.layout.spinnertext,context.getResources().getStringArray(R.array.min));
+        ArrayAdapter<String> Houradapter = new ArrayAdapter<String>(context, R.layout.spinnertext, context.getResources().getStringArray(R.array.hour));
+        ArrayAdapter<String> Minadapter = new ArrayAdapter<String>(context, R.layout.spinnertext, context.getResources().getStringArray(R.array.min));
 
         spHour.setAdapter(Houradapter);
         spMin.setAdapter(Minadapter);
@@ -51,34 +52,48 @@ public class DialogSelectTimer {
             public void onClick(View view) {
                 // '확인' 버튼 클릭시 메인 액티비티에서 설정한 timer_label에
                 // 커스텀 다이얼로그에서 입력한 메시지를 대입한다.
-                String time ="";
-                String strhour=spHour.getSelectedItem().toString();
-                String strmin=spMin.getSelectedItem().toString();
+                String time = "";
+                String strhour = spHour.getSelectedItem().toString();
+                String strmin = spMin.getSelectedItem().toString();
 
-                if(strhour.equals("0"))
-                    time=strmin+" 분";
-                else if (strmin.equals("0"))
-                    time=strhour+" 시간";
-                else
-                    time=strhour+" 시간 "+strmin+" 분";
+                int totalTime = (Integer.parseInt(spHour.getSelectedItem().toString()) * 60 + Integer.parseInt(spMin.getSelectedItem().toString()));
+                int plusTime = 0;
 
-                timer_label.setText(time);
-                timer_label.setTextColor(Color.BLACK);
-                upper_label.setVisibility(View.VISIBLE);
-
-                int totalTime = (Integer.parseInt(spHour.getSelectedItem().toString()) * 60 +Integer.parseInt(spMin.getSelectedItem().toString()));
-                if (totalTime % 30 == 0){
-                    plus_label.setText("+ "+(totalTime/30 - 1)*10 +"분");
+                if (totalTime == 0 ){
+                    plusTime = 0;
+                } else if(totalTime % 30 == 0) {
+                    plusTime = (totalTime / 30 - 1) * 10;
                 } else {
-                    plus_label.setText("+ "+(totalTime/30)*10+"분");
+                    plusTime = (totalTime / 30) * 10;
                 }
-                plus_label.setVisibility(View.VISIBLE);
+                Log.d("real", String.valueOf(totalTime+plusTime));
+                if (totalTime + plusTime > least) {
+                    AlertDialog.Builder alert_time = new AlertDialog.Builder(context);
+                    alert_time.setTitle("시간설정 오류");
+                    alert_time.setMessage("약속이 시작하는 시간이 현재시간 이전이므로 설정할 수 없습니다.");
+                    alert_time.setPositiveButton("확인", null);
+                    alert_time.create().show();
+                }
+                else {
+                    if (strhour.equals("0"))
+                        time = strmin + " 분";
+                    else if (strmin.equals("0"))
+                        time = strhour + " 시간";
+                    else
+                        time = strhour + " 시간 " + strmin + " 분";
 
-                Log.d("timer", spHour.getSelectedItem().toString());
+                    timer_label.setText(time);
+                    timer_label.setTextColor(Color.BLACK);
+                    upper_label.setVisibility(View.VISIBLE);
+
+                    plus_label.setText("+ "+plusTime+"분");
+                    plus_label.setVisibility(View.VISIBLE);
+                    Log.d("timer", spHour.getSelectedItem().toString());
 
 
-                // 커스텀 다이얼로그를 종료한다.
-                dlg.dismiss();
+                    // 커스텀 다이얼로그를 종료한다.
+                    dlg.dismiss();
+                }
             }
         });
     }
--- pro-miss/app/src/main/java/com/minsudongP/Fragment/AppointmentFragemnt.java
+++ pro-miss/app/src/main/java/com/minsudongP/Fragment/AppointmentFragemnt.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,29 +41,38 @@ import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import static com.minsudongP.SetDestinyActivity.result_code;
 
 
 public class AppointmentFragemnt extends Fragment implements OnMapReadyCallback {
 
+    static final int request_code = 1;
+
     long mNow;
     Date mDate;
+    EditText tvName;
     TextView tvDate;
     TextView tvTime;
-    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd");
-    static final int request_code = 1;
-    public String latitude;
-    public String longitude;
-    NaverMap naverMap;
     TextView tvTimer;
     TextView tvAddress;
-    String date_time = "00:00";
-    EditText text;
 
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd");
+    String date_time = "00:00";
+
+    NaverMap naverMap;
+
+    int m_year = -1;
+    int m_month = -1;
+    int m_date = -1;
+    int m_hour = -1;
+    int m_min = -1;
+    int m_timer = -1;
 
     @Nullable
     @Override
@@ -74,7 +84,7 @@ public class AppointmentFragemnt extends Fragment implements OnMapReadyCallback 
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), SetDestinyActivity.class);
-                startActivityForResult(intent,request_code);
+                startActivityForResult(intent, request_code);
             }
         });
 
@@ -90,21 +100,44 @@ public class AppointmentFragemnt extends Fragment implements OnMapReadyCallback 
         (view.findViewById(R.id.map)).setFocusable(false);
         (view.findViewById(R.id.map)).setEnabled(false);
 
-        text = view.findViewById(R.id.appointment_name);
+        tvName = view.findViewById(R.id.appointment_name);
 
         final TextView upperDate = view.findViewById(R.id.upper_date);
+        final TextView upperTime = view.findViewById(R.id.upper_time);
+        final TextView upperTimer = view.findViewById(R.id.upper_timer);
+        final TextView plusTime = view.findViewById(R.id.appointment_plustime);
+
+        // 날짜
+        final Calendar cal = Calendar.getInstance();
         tvDate = view.findViewById(R.id.appointment_date);
         tvDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Calendar cal = Calendar.getInstance();
 
                 DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int year, int month, int date) {
-                        tvDate.setText(String.format("%02d-%02d-%02d", year, month + 1, date));
-                        tvDate.setTextColor(Color.BLACK);
-                        upperDate.setVisibility(View.VISIBLE);
+                        if (m_year!=year || m_month!=month || m_date!=date) {
+                            tvDate.setText(String.format("%02d-%02d-%02d", year, month + 1, date));
+                            tvDate.setTextColor(Color.BLACK);
+                            upperDate.setVisibility(View.VISIBLE);
+                            m_year = year;
+                            m_month = month;
+                            m_date = date;
+
+                            // 시간, 타이머 초기화
+                            tvTime.setText("시간");
+                            tvTime.setTextColor(getResources().getColor(R.color.colorAccent));
+                            upperTime.setVisibility(View.INVISIBLE);
+                            tvTimer.setText("약속장소로 언제 출발하시나요?");
+                            tvTimer.setTextColor(getResources().getColor(R.color.colorAccent));
+                            upperTimer.setVisibility(View.INVISIBLE);
+                            plusTime.setText("");
+                            m_hour = -1;
+                            m_min = -1;
+                            m_timer = -1;
+                            Toast.makeText(getContext(),m_year+"년 "+(m_month+1)+"월 "+m_date+"일\n"+m_hour+"시 "+m_date+"",Toast.LENGTH_LONG).show();
+                        }
                     }
                 }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
 
@@ -116,46 +149,61 @@ public class AppointmentFragemnt extends Fragment implements OnMapReadyCallback 
 
 
         // 시간 설정
-        final TextView upperTime = view.findViewById(R.id.upper_time);
         tvTime = view.findViewById(R.id.appointment_time);
         tvTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Calendar cal = Calendar.getInstance();
+                // 날짜 선택 먼저 하도록 유도
+                if (m_year==-1 && m_month==-1 && m_date==-1) {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                    alert.setTitle("날짜 미설정");
+                    alert.setMessage("약속 날짜를 먼저 선택해 주세요.");
+                    alert.setPositiveButton("확인", null);
+                    alert.create().show();
+                    return;
+                }
+
                 TimePickerDialog dialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int hour, int min) {
 
                         int currentHour = cal.get(Calendar.HOUR_OF_DAY);
-
                         int currentMin = cal.get(Calendar.MINUTE);
-                        if (currentHour < hour || currentHour == hour && currentMin < min) {
-                            date_time = hour + ":" + min;
-                            String ampm = "AM";
 
-                            if (hour >= 12) {
-                                ampm = "PM";
-                                if (hour != 12) hour -= 12;
-                            }
+                        // 현재시간으로부터 1시간 이후에만 약속 생성
+                        if (m_year == cal.get(Calendar.YEAR) && m_month==cal.get(Calendar.MONTH) && m_date==cal.get(Calendar.DATE)
+                                && (currentHour + 1 > hour || currentHour + 1 == hour && currentMin > min)) {
+                            AlertDialog.Builder alert_time = new AlertDialog.Builder(getContext());
+                            alert_time.setTitle("시간설정 오류");
+                            alert_time.setMessage("현재 시간으로부터 1시간 이후의 약속만 생성 가능합니다.");
+                            alert_time.setPositiveButton("확인", null);
+                            alert_time.create().show();
+                            return;
+                        }
 
+                        // 제약사항에 걸리지 않으면 바로 생성
+
+                        date_time = hour + ":" + min;
+                        String ampm = "AM";
+                        if (hour >= 12) {
+                            ampm = "PM";
+                            if (hour != 12) hour -= 12;
+                        }
+
+                        if (m_hour!=hour || m_min != min){
                             tvTime.setText(String.format("%s %02d : %02d", ampm, hour, min));
                             tvTime.setTextColor(Color.BLACK);
                             upperTime.setVisibility(View.VISIBLE);
-                        } else {
-                            AlertDialog.Builder alert_time = new AlertDialog.Builder(getContext());
+                            m_hour = hour;
+                            if(ampm.equals("PM")) m_hour+=12;
+                            m_min = min;
 
-                            alert_time.setTitle("시간설정 오류");
-                            alert_time.setMessage("현재시간 보다 이전시간으로 설정하셨습니다. 다시 설정하세요");
-
-                            alert_time.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-
-                                }
-                            });
-
-                            AlertDialog alert = alert_time.create();
-                            alert.show();
+                            tvTimer.setText("약속장소로 언제 출발하시나요?");
+                            tvTimer.setTextColor(getResources().getColor(R.color.colorAccent));
+                            upperTimer.setVisibility(View.INVISIBLE);
+                            plusTime.setText("");
+                            m_timer=-1;
                         }
                     }
                 }, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false);//마지막 boolean 값은 시간을 24시간으로 보일지 아닐지
@@ -170,19 +218,26 @@ public class AppointmentFragemnt extends Fragment implements OnMapReadyCallback 
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), SetDestinyActivity.class);
-                startActivityForResult(intent,request_code);
+                startActivityForResult(intent, request_code);
             }
         });
 
         // 타이머 설정
         tvTimer = view.findViewById(R.id.appointment_timer);
-        final TextView upperTimer = view.findViewById(R.id.upper_timer);
-        final TextView plusTime = view.findViewById(R.id.appointment_plustime);
         View.OnClickListener timerListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DialogSelectTimer dialogSelectTimer = new DialogSelectTimer(getActivity());
-                dialogSelectTimer.callFunction(tvTimer,upperTimer,plusTime);
+                if (m_hour==-1 || m_min==-1) {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                    alert.setTitle("시간 미설정");
+                    alert.setMessage("약속 시간을 먼저 선택해 주세요.");
+                    alert.setPositiveButton("확인", null);
+                    alert.create().show();
+                } else {
+                    Log.d("gap",String.valueOf(getGap()));
+                    DialogSelectTimer dialogSelectTimer = new DialogSelectTimer(getActivity());
+                    dialogSelectTimer.callFunction(tvTimer, upperTimer, plusTime,getGap());
+                }
             }
         };
 
@@ -209,7 +264,6 @@ public class AppointmentFragemnt extends Fragment implements OnMapReadyCallback 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         this.naverMap = naverMap;
-
         naverMap.setMaxZoom(14.0);
         naverMap.setMinZoom(14.0);
 //        this.naverMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
@@ -236,7 +290,7 @@ public class AppointmentFragemnt extends Fragment implements OnMapReadyCallback 
                 if (!address.equals("")) {
                     tvAddress.setText(address);
                     tvAddress.setTextColor(Color.BLACK);
-                    ((TextView)getView().findViewById(R.id.upper_address)).setVisibility(View.VISIBLE);
+                    ((TextView) getView().findViewById(R.id.upper_address)).setVisibility(View.VISIBLE);
                 }
             }
         }
@@ -251,7 +305,7 @@ public class AppointmentFragemnt extends Fragment implements OnMapReadyCallback 
     public int getTimer() {
         String s = tvTimer.getText().toString();
 
-        if (tvTimer.getText().equals("약속장소로 언제 출발하시나요?")){
+        if (tvTimer.getText().equals("약속장소로 언제 출발하시나요?")) {
             return -1;
         } else {
             s = s.replaceAll(" ", "");
@@ -274,8 +328,31 @@ public class AppointmentFragemnt extends Fragment implements OnMapReadyCallback 
 
 
     public void SendDatatoActivity() {
-        ((appointment) getActivity()).setAppointment_role_1(text.getText().toString(), "" + naverMap.getCameraPosition().target.latitude, "" + naverMap.getCameraPosition().target.longitude
+        ((appointment) getActivity()).setAppointment_role_1(tvName.getText().toString(), "" + naverMap.getCameraPosition().target.latitude, "" + naverMap.getCameraPosition().target.longitude
                 , "" + getTimer(), tvDate.getText().toString(), date_time);
+    }
+
+    public int getGap(){
+        Calendar currentTime = Calendar.getInstance();
+
+        try {
+            SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy.MM.dd kk:mm");
+
+            String current = String.format("%04d.%02d.%02d %02d:%02d", currentTime.get(Calendar.YEAR), currentTime.get(Calendar.MONTH), currentTime.get(Calendar.DATE),currentTime.get(Calendar.HOUR_OF_DAY),currentTime.get(Calendar.MINUTE));
+            String appoint = String.format("%04d.%02d.%02d %02d:%02d", m_year, m_month, m_date, m_hour, m_min);
+
+            Date startDate = dataFormat.parse(current);
+            Date endDate = dataFormat.parse(appoint);
+
+            long duration = endDate.getTime() - startDate.getTime();
+            String tag = current+"와 "+appoint+"의 차이";
+            Log.d(tag, String.valueOf((int)duration/60000));
+            return (int)duration/60000;
+        }catch (ParseException e){
+
+        }
+
+        return -1;
     }
 
 }
